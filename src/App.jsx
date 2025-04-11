99e120eb299730852db3797528c4d79630a7e678
Silali <gibsonsilali@gmail.com>
2025-04-11 10:27:38 +0300

    fetch devices

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,54 @@
-import './App.css'
-import {useWorker} from "@koale/useworker";
-import {detectUsb} from "./usb/index.js";
+import { useState } from 'react';
 
 function App() {
-  const [usbWorker] = useWorker(detectUsb)
+  const [devices, setDevices] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   const fetchUsb = async () => {
-    const _ = await usbWorker()
-  }
+    setIsFetching(true);
+    try {
+      const device = await navigator.usb.requestDevice({
+        filters: [{ vendorId: '1321' }],
+      });
+
+      if (!devices.some((device) => device.vendorId.toString() === '1321')) {
+        setDevices([...devices, device]);
+      }
+
+    } catch (e) {
+      alert(`Error fetching device: ${e.message}`);
+    } finally {
+      setIsFetching(false);
+    }
+  };
 
   return (
-    <>
-      <h1>Usb Detector</h1>
-      <button onClick={fetchUsb}>Click</button>
-    </>
-  )
+    <div className={'flex justify-center items-center h-screen'}>
+      <div className="border p-4 w-1/2">
+        <button className={'bg-slate-700 text-white py-2 px-4'}
+                onClick={fetchUsb}>{isFetching ? 'Fetching...' : 'Request Devices'}</button>
+        <hr className={'my-4'} />
+        <table className={'w-full'}>
+          <thead>
+          <tr align={'left'}>
+            <th>Product</th>
+            <th>Manufacturer</th>
+            <th></th>
+          </tr>
+          </thead>
+          <tbody>
+          {devices.map((device, index) => (
+            <tr className={'text-sm border-b  border-slate-500'} key={index}>
+              <td className={'pr-4 py-2'}>{device.productName.trim()}</td>
+              <td className={'pr-4 py-2'}>{device.manufacturerName.trim()}</td>
+              <td className={'pr-4 py-2'}></td>
+            </tr>
+          ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
